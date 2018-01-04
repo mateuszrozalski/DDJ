b9e31353b0c8ab5864c006a346605a1ccdb76c47
Mateusz Różalski <olivetum@gmail.com>
2018-01-04 23:11:28 +0100

    set mobile menu css

--- js/bulma.js
+++ js/bulma.js
@@ -2,9 +2,20 @@
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
     var burger = document.querySelector('.nav-toggle');
-    var menu = document.querySelector('.nav-menu');
+    var menu = document.querySelector('.menu');
+    var menuItem = document.querySelectorAll('.menu-item');
     burger.addEventListener('click', function() {
         burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
+        menu.classList.toggle('is-hidden-mobile');
+        menu.classList.toggle('mobile-menu');
     });
+    for (i = 0; i < menuItem.length; i++) {
+      menuItem[i].addEventListener('click', function() {
+          console.log('lol')
+          burger.classList.toggle('is-active');
+          menu.classList.toggle('is-hidden-mobile');
+          menu.classList.toggle('mobile-menu');
+      });
+    }
+
 })();
